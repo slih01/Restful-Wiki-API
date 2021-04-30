34c27920ba7a476ef27a2b9c3f6482acdc10204a
Darren Haz <darrenhazan@gmail.com>
2021-04-30 18:24:03 +0100

    first and final commit

---
+++ app.js
@@ -0,0 +1,118 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+const articleSchema = {
+  title: String,
+  content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+
+
+///////////////////// Request targetting ALL artices //////////////////////
+
+app.route("/articles")
+.get(function(req,res){
+
+  Article.find({}, function(err,foundArticles){
+    if(!err) {
+          res.send(foundArticles);
+    } else {
+  res.send(err);
+    }
+  })
+})
+.post(function(req,res){
+  const newArticle = new Article({
+    title: req.body.title,
+  content: req.body.content
+});
+newArticle.save(function(err){
+  if (!err) {
+    res.send("Sent successfully")
+  } else {
+    res.send(err)
+  };
+});
+})
+.delete(function(req,res){
+  Article.deleteMany({}, function(err,foundArticles){
+    if (!err) {
+      res.send("Items successfully deleted");
+    } else {
+      res.send(err);
+    };
+  });
+});
+
+
+//////////////// Request targetting specific articles ////////////////
+
+app.route("/articles/:articleName")
+
+.get(function(req,res){
+
+  Article.findOne({title: req.params.articleName},function(err,foundArticle){
+    if (foundArticle) {
+      res.send(foundArticle)
+    } else {
+      res.send("Not found");
+    };
+  });
+})
+
+.put(function(req,res){
+  Article.update(
+    {title: req.params.articleName},
+    {title: req.body.title, content: req.body.content}, {overwrite:true},
+    function(err,updatedArticle){
+      if(!err){
+        res.send(updatedArticle)
+      } else {
+        res.send("Article not updated")
+      };
+    });
+})
+.patch(function(req,res){
+  Article.update(
+    {title: req.params.articleName},
+  {$set: req.body},
+    function(err,updatedArticle){
+      if(!err){
+        res.send(updatedArticle)
+      } else {
+        res.send("Article not updated")
+      };
+    });
+})
+.delete(function(req,res){
+  Article.deleteOne({title: req.params.articleName}, function(err){
+    if(!err) {
+      res.send("article deleted")
+    } else {
+      res.send("No such article");
+    };
+  });
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
